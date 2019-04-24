ec452e075b5d0496d956b3fc94e86db96e681a22
zhangjie <357688981@qq.com>
2019-04-24 19:55:24 +0800

    test 着色器

---
+++ LearnOpenGL/LearnOpenGL/Shaders/shader.h
@@ -0,0 +1,23 @@
+#ifndef SHADERS_SHADER_H
+#define SHADERS_SHADER_H
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <glad\glad.h>
+
+class Shader
+{
+public:
+    Shader(const char* vertexPath, const char* fragmentPath);
+    void use();
+
+private:
+    void checkCompileErrors(unsigned int shader, const std::string& type);
+
+private:
+    unsigned int ID;
+};
+
+#endif  // SHADERS_SHADER_H
