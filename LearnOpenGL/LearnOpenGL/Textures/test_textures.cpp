5337c3a78ecf9578e14ec8127e790a87886ade2f
zhangjie <357688981@qq.com>
2019-07-06 14:08:56 +0800

    纹理(Textures)

---
+++ LearnOpenGL/LearnOpenGL/Textures/test_textures.cpp
@@ -0,0 +1,192 @@
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shader.h"
+#include <stb_image.h>
+
+#pragma comment(lib, "glfw3.lib")
+
+
+// 当用户改变窗口的大小的时候，视口也应该被调整。
+// 我们可以对窗口注册一个回调函数, 它会在每次窗口大小被调整的时候被调用
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) 
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+    // 初始化GLFW
+    glfwInit();
+    // 配置GLFW
+    // 告诉GLFW我们使用的OpenGL版本是3.3, 将主版本号(Major)和次版本号(Minor)都设为3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // 告诉GLFW我们使用的是核心模式(Core-profile)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // 创建一个窗口对象, 这个窗口对象存放了所有和窗口相关的数据, 而且会被GLFW的其他函数频繁地用到
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Test textures", NULL, NULL);
+    if (window == NULL) {
+        printf("Failed to create GLFW window!\n");
+        glfwTerminate();
+        return -1;
+    }
+    // 创建完窗口通知GLFW将我们窗口的上下文设置为当前线程的主上下文
+    glfwMakeContextCurrent(window);
+
+    // GLAD是用来管理OpenGL的函数指针的, 所以在调用任何OpenGL的函数之前我们需要初始化GLAD
+    // 给GLAD传入用来加载系统相关的OpenGL函数指针地址的函数glfwGetProcAddress
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Failed to initialize GLAD!\n");
+        return -1;
+    }
+
+    // 告诉OpenGL渲染窗口的尺寸大小, 即视口(Viewport)
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+    // 注册这个回调函数, 告诉GLFW我们希望每当窗口调整大小的时候调用这个函数
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    Shader shader("E:/SourceCodes/MyGithub/CG/LearnOpenGL/LearnOpenGL/Textures/shader.vs", "E:/SourceCodes/MyGithub/CG/LearnOpenGL/LearnOpenGL/Textures/shader.fs");
+
+    // set up vertex data and configure vertex attributes
+    float vertices[] = {
+        // positions         // colors           // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,  // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f   // top left 
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,  // first triangle
+        1, 2, 3   // second triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    // 把新创建的缓冲绑定到GL_ARRAY_BUFFER目标上
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // 把之前定义的顶点数据复制到缓冲的内存中
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // 告诉OpenGL该如何解析顶点数据
+    // 位置属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    // 以顶点属性位置值作为参数，启用顶点属性
+    glEnableVertexAttribArray(0);
+    
+    // 颜色属性
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // 纹理属性
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // load and create a texture
+    unsigned int texture1, texture2;
+    // texture1
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_REPEAT);
+
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load("E:/SourceCodes/MyGithub/CG/LearnOpenGL/LearnOpenGL/Textures/container.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture!" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // texture2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    data = stbi_load("E:/SourceCodes/MyGithub/CG/LearnOpenGL/LearnOpenGL/Textures/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture!" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // tell opengl for each sampler to which texture unit it belongs to
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
+
+    // 渲染循环(Render Loop)
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        processInput(window);
+
+        // 清空屏幕的颜色缓冲
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        // draw triangle
+        // 用创建的着色器程序对象作为它的参数, 以激活这个对象
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        // 检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态,并调用对应的回调函数
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    // 当渲染循环结束后我们需要正确释放/删除之前的分配的所有资源
+    glfwTerminate();
+
+    return 0;
+}
