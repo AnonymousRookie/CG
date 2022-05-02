bec930dc75c8b1bf56a34a00ebd75d9b0a587026
zj <coderzj@outlook.com>
2022-05-02 18:10:13 +0800

    update 作业3 使用双线性插值进行纹理采样

--- GAMES101/Homework3/Texture.hpp
+++ GAMES101/Homework3/Texture.hpp
@@ -30,5 +30,51 @@ public:
         return Eigen::Vector3f(color[0], color[1], color[2]);
     }
 
+    // 使用双线性插值进行纹理采样
+    Eigen::Vector3f getColorBilinear(float u, float v)
+    {		
+        if (u < 0) u = 0;
+        if (u > 1) u = 1;
+        if (v < 0) v = 0;
+        if (v > 1) v = 1;
+
+        auto u_img = u * width;
+        auto v_img = (1 - v) * height;
+
+        float u_min = std::floor(u_img);
+        float u_max = std::min((float)width, std::ceil(u_img));
+        float v_min = std::floor(v_img);
+        float v_max = std::min((float)height, std::ceil(v_img));
+
+        /*
+             u1
+        u01------u11
+        |         |
+        |         |
+        |         |
+        u00------u10
+             u0
+        */
+
+        // opencv坐标的高和uv坐标和高是相反的
+        auto u00 = image_data.at<cv::Vec3b>(v_max, u_min);
+        auto u10 = image_data.at<cv::Vec3b>(v_max, u_max);
+        auto u01 = image_data.at<cv::Vec3b>(v_min, u_min);
+        auto u11 = image_data.at<cv::Vec3b>(v_min, u_max);
+
+
+        // k = (x-x0) / (x1-x0) = (y-y0) / (y1-y0)
+        // x = (1-k)*x0 + k*x1     
+        // y = (1-k)*y0 + k*y1     
+
+        auto uk = (u_img - u_min) / (u_max - u_min);
+        auto u0 = (1 - uk) * u00 + uk * u10;
+        auto u1 = (1 - uk) * u01 + uk * u11;
+
+        auto vk = (v_img - v_max) / (v_min - v_max);
+        auto p = (1 - vk) * u0 + vk * u1;
+
+        return Eigen::Vector3f(p[0], p[1], p[2]);
+    }   
 };
 #endif //RASTERIZER_TEXTURE_H
