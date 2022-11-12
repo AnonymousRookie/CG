c1c5cc2590ce9db1e78f844622bfacfb45b90981
zj <coderzj@outlook.com>
2022-11-12 20:23:28 +0800

    Homework4 抗锯齿

--- GAMES101/Homework4/main.cpp
+++ GAMES101/Homework4/main.cpp
@@ -81,16 +81,23 @@ void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
     */
 
     // 反走样, 对于一个曲线上的点，不只把它对应于一个像素，需要根据到像素中心的距离来考虑与它相邻的像素的颜色。
+    // 每得到一个曲线上的点时，就根据它到自己周围的3*3个像素中心的距离dist来为这些像素填色，以达到平滑过渡的效果。
+    // 每个像素的颜色是255*ratio，dist的范围是[0, 3/sqrt(2)]，ratio的范围是[0, 1]，
+    // 则ratio关于dist的函数就是ratio = 1 - std::sqrt(2) / 3.0 * dist
+    // 重复计算的点就按照该点的颜色最大值算。
     for (double t = 0.0; t <= 1.0; t += 0.001) 
     {
         auto point = recursive_bezier(control_points, t);
-        for (int i = 0; i <= 1; ++i) {
-            for (int j = 0; j <= 1; ++j) {
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
                 int x = point.x + i;
                 int y = point.y + j;
 
                 double dist = std::sqrt(std::pow(point.x - x, 2) + std::pow(point.y - y, 2));
-                window.at<cv::Vec3b>(y, x)[1] = std::min(window.at<cv::Vec3b>(y, x)[1] + 255 * std::max(2-exp(dist), 0.0), 255.0);
+                double ratio = 1 - std::sqrt(2) / 3.0 * dist;
+                
+                double lastClr = window.at<cv::Vec3b>(y, x)[1];
+                window.at<cv::Vec3b>(y, x)[1] = std::max(lastClr, 255.0 * ratio);
             }
         }
     }
@@ -114,6 +121,7 @@ int main()
 
         if (control_points.size() == 4) 
         {
+            // 同时调用 naive_bezier 和 bezier 函数，如果实现正确，则两者均应写入大致相同的像素，因此该曲线将表现为黄色
             naive_bezier(control_points, window);
             bezier(control_points, window);
 
